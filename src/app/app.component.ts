f453e3d9fbf9f089194a231df4d64aca20993f30
= <=>
2024-02-16 16:47:23 -0300

    Feat: Add Window Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from './services/translate.service';
+import { WindowService } from './services/window.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,20 @@ import { TranslateService } from './services/translate.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private translateService: TranslateService) {}
+  constructor(
+    private translateService: TranslateService,
+    private windowService: WindowService
+  ) {}
+
+  public isMobile: boolean =
+    window.innerWidth < this.windowService.MOBILE_WIDTH ? true : false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth < this.windowService.MOBILE_WIDTH)
+      this.windowService.isMobile.next(true);
+    else this.windowService.isMobile.next(false);
+  }
 
   ngOnInit(): void {
     this.translateService.use(navigator.language);
